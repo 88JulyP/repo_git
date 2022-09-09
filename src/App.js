3a8cfa4a667839b5215e4d00cf3027359d62056a
pablorusso25 <pablitorusso25@gmail.com>
2022-09-08 22:28:45 -0300

    subir carusell

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Carusell from "./Componentes/Carusell";
+
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline py-2 text-center">
-        <p>Subir Tailwind a rama principal</p>
-    </h1>
-
+    <>
+      <h1 className="text-3xl font-bold underline py-2 text-center">
+          <p>Subir Tailwind a rama principal</p>
+      </h1>
+      <Carusell/>
+    </>
   );
 }
 
