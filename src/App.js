75b31f39cbd48e09add7d70b205e06138f0638e8
pablorusso25 <pablitorusso25@gmail.com>
2022-09-08 22:01:37 -0300

    se agrega tailwind-elements y Carussel

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@
 function App() {
   return (
     <h1 className="text-3xl font-bold underline py-2 text-center">
-      <p>Subir Tailwind a rama principal</p>
+        <p>Subir Tailwind a rama principal</p>
     </h1>
+
   );
 }
 
